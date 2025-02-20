bf6d9b844e59e2372c49125ec66a9a448387ede7
Nolan5272 <njf5272@gmail.com>
2025-02-20 07:45:06 -0500

    added ndonut.c

---
+++ ndonut.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(){
+
+    int x = 0;
+    double y = 1.0;
+
+
+    float R1 = 1;
+    float R2 = 2;
+
+    float theta;
+    float phi;
+    while(x = 0){
+
+    }
+
+}
